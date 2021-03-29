2ce13eff8236b7b1bea2b895221b6287cbb7bf9c
Justin <jmp86@yahoo.com>
2021-03-28 22:11:07 -1000

    added wedding photo,added js switch

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
 
-document.querySelector('body').appendChild(h2);
+let fhobby = document.getElementById("input");
+
+function favoriteHobby() {
+let greeting;
+
+switch (fhobby) {
+  case "Golf":
+    greeting = "Golf is hard!";
+    break;
+  case "Video games":
+    greeting = "Wish I had more time to play!";
+    break;
+  case "Going to the beach":
+    greeting = "I need to work on my tan!";
+    break;
+  case "Exercising":
+    greeting = "I need to work out more!";
+    break;
+  case "Coding":
+        greeting = "I can't wait to learn more!";
+        break;
+  default:
+    greeting = "I should try that!";
+
+    alert (greeting);
+}
+}
+
+fhobby.addEventListener('submit', (event) => {
+    favoriteHobby();
+});
+
